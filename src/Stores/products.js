670f8af400fb04b5b79e832a520dbc1df5d86691
ItIsEternity <lavrovslava207@gmail.com>
2021-10-10 17:06:46 +0300

    Fix of URL's paths

--- src/Stores/products.js
+++ src/Stores/products.js
@@ -4,7 +4,7 @@ export const products = writable([
     {
         id: 1,
         name: 'Nike Air VaporMax 2021 FK',
-        image: '/img/кроссовки-air-vapormax-2021-fk-Tk4KhJ.jfif',
+        image: 'img/кроссовки-air-vapormax-2021-fk-Tk4KhJ.jfif',
         selected: false,
         category: 'Кроссовки',
         show: true
@@ -12,7 +12,7 @@ export const products = writable([
     {
         id: 2,
         name: 'Nike Air Max 96 II',
-        image: '/img/кроссовки-air-max-96-2-6dHbwR.jfif',
+        image: 'img/кроссовки-air-max-96-2-6dHbwR.jfif',
         selected: false,
         category: 'Кроссовки',
         show: true
@@ -20,7 +20,7 @@ export const products = writable([
     {
         id: 3,
         name: 'Nike Air Max 90',
-        image: '/img/кроссовки-air-max-90-S8L0TL.jfif',
+        image: 'img/кроссовки-air-max-90-S8L0TL.jfif',
         selected: false,
         category: 'Кроссовки',
         show: true
@@ -28,7 +28,7 @@ export const products = writable([
     {
         id: 4,
         name: 'Nike Air Max 97',
-        image: '/img/кроссовки-air-max-97-78Q3Q7.jfif',
+        image: 'img/кроссовки-air-max-97-78Q3Q7.jfif',
         selected: false,
         category: 'Кроссовки',
         show: true
@@ -36,7 +36,7 @@ export const products = writable([
     {
         id: 5,
         name: 'Philadelphia 76ers',
-        image: '/img/футболка-с-логотипом-нба-dri-fit-philadelphia-76ers-NMpxKF.jfif',
+        image: 'img/футболка-с-логотипом-нба-dri-fit-philadelphia-76ers-NMpxKF.jfif',
         selected: false,
         category: 'Футболки',
         show: true
@@ -44,7 +44,7 @@ export const products = writable([
     {
         id: 6,
         name: 'Jazz',
-        image: '/img/футболка-нба-jazz-X7T63R.jfif',
+        image: 'img/футболка-нба-jazz-X7T63R.jfif',
         selected: false,
         category: 'Футболки',
         show: true
@@ -52,7 +52,7 @@ export const products = writable([
     {
         id: 7,
         name: 'Celtics',
-        image: '/img/футболка-нба-celtics-0sBqmQ.jfif',
+        image: 'img/футболка-нба-celtics-0sBqmQ.jfif',
         selected: false,
         category: 'Футболки',
         show: true
